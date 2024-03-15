513a1c2f8cce9851a7c49da929fafe683b1dbd71
soarnsoar <soarnsoar@gmail.com>
2024-03-15 15:14:50 +0900

    wrapup

--- test.py
+++ test.py
@@ -2,13 +2,10 @@
 #ROOT.gSystem.Load("/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.3/lib/libLHAPDF.so")
 import lhapdf
 from math import sqrt
-def GetStoSbar(x,q2,mypdf,lhapdf_name):
-    p = mypdf.mkPDF(lhapdf_name)
-    #Q2=1000
-    #x=0.3
+def GetStoSbar(x,q2,lhapdf_name):
+    p = lhapdf.mkPDF(lhapdf_name)
     pdgid=3
     Nrep=p.set().size
-    #print "Nrep",Nrep
     #--nominal
     irep=0
     s_pdf=p.xfxQ2(pdgid,x,q2)
@@ -16,7 +13,7 @@ def GetStoSbar(x,q2,mypdf,lhapdf_name):
     r_nom=s_pdf/sbar_pdf
     sum_dr2=0
     for irep in range(Nrep):
-        p = mypdf.mkPDF(lhapdf_name,irep)
+        p = lhapdf.mkPDF(lhapdf_name,irep)
         
         s_pdf=p.xfxQ2(pdgid,x,q2)
         sbar_pdf=p.xfxQ2(-pdgid,x,q2)
@@ -30,12 +27,13 @@ def GetStoSbar(x,q2,mypdf,lhapdf_name):
     return r_nom,r_err
 
 Q2=1000
-#lhapdf_name="NNPDF31_nnlo_hessian_pdfas"
-lhapdf_name="CT18NNLO"
-mypdf=lhapdf
-mypdf.setVerbosity(0)
+lhapdf_name="NNPDF31_nnlo_hessian_pdfas"
+#lhapdf_name="CT18ZNNLO"
+#lhapdf_name="CT18NNLO"
+lhapdf.setVerbosity(0)
+print "<",lhapdf_name,">", "Q2=",Q2
 print "x",'\t',"s/s~","\t","err(s/s~)"
 print "--------------------"
 for x in [0.3,0.25,0.2,0.1,0.01,0.001,0.0001,0.00001,1e-6,1e-7,1e-8,1e-9,1e-10,1e-11]:
-    r,r_err=GetStoSbar(x,Q2,mypdf,lhapdf_name)
-    print x,'\t', r,r_err
+    r,r_err=GetStoSbar(x,Q2,lhapdf_name)
+    print x,'\t', r,"\t",r_err
