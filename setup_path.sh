b89992ce2ab89e822d71986d0a19917ab8db506d
Junho <soarnsoar@gmail.com>
2024-04-01 11:07:18 +0900

    set cmssw env

--- setup_path.sh
+++ setup_path.sh
@@ -1,1 +1,16 @@
-export PYTHONPATH=$PYTHONPATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.3/lib/python2.7/site-packages/
+#export PATH=$PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.3/bin/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.3/lib/
+export CMS_PATH=/cvmfs/cms.cern.ch
+source $CMS_PATH/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc820
+export cmsswrel='cmssw/CMSSW_10_6_4' ##
+
+cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/$cmsswrel/src
+echo "@@@@ SCRAM_ARCH = "$SCRAM_ARCH
+echo "@@@@ cmsswrel = "$cmsswrel
+echo "@@@@ scram..."
+eval `scramv1 runtime -sh`
+cd -
+source /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/$cmsswrel/external/$SCRAM_ARCH/bin/thisroot.sh
+
+export PYTHONPATH=$PYTHONPATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/lhapdf/6.2.3/lib/python2.7/site-packages/
